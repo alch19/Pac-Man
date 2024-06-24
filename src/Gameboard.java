d0e8e3d1b08a55a506de7a41837d63d1fff002db
Brian Farkas <brianfarkas75@gmail.com>
2024-06-23 20:02:56 -0500

    implemented pacman moving and basic board that i can randomize in the future

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -1,14 +1,36 @@
-import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.Graphics;
-import java.util.Timer;
-import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Gameboard extends JPanel implements ActionListener{
     private Timer timer;
+    private int pacManX;
+    private int pacManY;
+    private int pacManDX;
+    private int pacManDY;
+    private final int[][] maze = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
+        {1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    public Gameboard() {
+        initBoard();
+        pacManX=40;
+        pacManY=40;
+        pacManDX=0;
+        pacManDY=0;
+    }
 
     private void initBoard() {
         setBackground(Color.BLACK);
@@ -22,19 +44,79 @@ public class Gameboard extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        movePacMan();
         repaint();
     }
 
-    private void paintComponent(Graphics g) {
+    private void movePacMan() {
+        int newPacManX = pacManX + pacManDX;
+        int newPacManY = pacManY + pacManDY;
+
+        if (isValidMove(newPacManX, newPacManY)) {
+            pacManX = newPacManX;
+            pacManY = newPacManY;
+        }
+    }
+
+    private boolean isValidMove(int x, int y) {
+        int gridX=x/40;
+        int gridY=y/40;
+
+        if (maze[gridY][gridX] == 1) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawGame(g);
     }
 
     private void drawGame(Graphics g) {
-        
+        drawMaze(g);
+        drawPacMan(g);
     }
 
-    private class KAdapter() {
+    private void drawMaze(Graphics g) {
+        for (int y = 0; y < maze.length; y++) {
+            for (int x = 0; x < maze[0].length; x++) {
+                if (maze[y][x] == 1) {
+                    g.setColor(Color.BLUE);
+                    g.fillRect(x * 40, y * 40, 40, 40);
+                } else if (maze[y][x] == 0) {
+                    g.setColor(Color.YELLOW);
+                    g.fillOval(x * 40 + 15, y * 40 + 15, 10, 10);
+                }
+            }
+        }
+    }
 
+    private void drawPacMan(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(pacManX, pacManY, 40, 40);
+    }
+
+
+    private class KAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if (key == KeyEvent.VK_LEFT) {
+                pacManDX = -40;
+                pacManDY = 0;
+            } else if (key == KeyEvent.VK_RIGHT) {
+                pacManDX = 40;
+                pacManDY = 0;
+            } else if (key == KeyEvent.VK_UP) {
+                pacManDX = 0;
+                pacManDY = -40;
+            } else if (key == KeyEvent.VK_DOWN) {
+                pacManDX = 0;
+                pacManDY = 40;
+            }
+        }
     }
 }
